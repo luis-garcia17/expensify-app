2fd284bb0942586d78afe2164719cb3fca4acf62
Luis Garcia <luis.lag17@gmail.com>
2019-03-28 02:27:59 -0600

    Setup production build and server

--- src/tests/fixtures/filters.js
+++ src/tests/fixtures/filters.js
@@ -14,4 +14,4 @@ const altFilters = {
     endDate: moment(0).add(3, 'days')
 };
 
-export {filters, altFilters}; //This is just another way of exporting named exports.
+export { filters, altFilters }; //This is just another way of exporting named exports.
