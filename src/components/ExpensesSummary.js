73b360696288ca0730eaef44515555a8e5e5510f
Luis Garcia <luis.lag17@gmail.com>
2019-07-07 23:56:45 -0500

    Added styles for header and inputs

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,13 +3,19 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} with a total of {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span> {expenseCount} </span> {expenseWord} with a total of <span> {formattedExpensesTotal} </span></h1>
+                <div className="page-header__actions">
+                    <Link to="/create" className="button">Add expense</Link>                
+                </div>
+            </div>
         </div>
     );   
 };
--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -3,5 +3,5 @@ export default expenses => {
     return expenses.map(expense => expense.amount).reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 };
 
-/* We used .map in here to turn an array of objects into an array of numbers, I tried other things and they didn-t work so I suppose
+/* We used .map in here to turn an array of objects into an array of numbers, I tried other things and they didn't work so I suppose
 this is the best method to do that */
